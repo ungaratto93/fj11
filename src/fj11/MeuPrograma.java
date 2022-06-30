384cca9687455158e935c0a8d24e50a369f0068d
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-06-30 20:58:49 -0300

    3. Sabendo que os caracteres \n representam uma quebra delinhas,imprima duas linhas detexto usando uma única linha decódigoSystem.out.

--- src/fj11/MeuPrograma.java
+++ src/fj11/MeuPrograma.java
@@ -4,5 +4,6 @@ public class MeuPrograma {
 	public static void main(String[] args) {
 		System.out.println("Minha primeira aplicação Java!");
 		System.out.println("Olá Mundo!");
+		System.out.println("Meu nome é \nEduardo");
 	}
 }

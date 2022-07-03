d82a9c0a58fdc94923f65409d025f1d4d1e21873
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-07-03 14:32:14 -0300

    2. Imprima a soma de 1 até 1000.

---
+++ src/cap3/Exercicio2.java
@@ -0,0 +1,11 @@
+package cap3;
+
+public class Exercicio2 {
+	public static void main(String[] args) {
+		int soma = 0;
+		for(int index = 1; index <= 3; index++) {
+			System.out.println(soma += index);
+		}
+	}
+	
+}

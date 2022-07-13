615a72b4c251707f086296b096b985bd8cf53cce
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-07-12 21:09:49 -0300

    1. Modele uma conta

---
+++ src/cap4/Conta.java
@@ -0,0 +1,19 @@
+package cap4;
+
+public class Conta {
+	String nomeTitular;
+	int numero;
+	String agencia;
+	double saldo;
+	String dataAbertura;
+
+	public void sacar(double quantiade) {
+	};
+
+	public void depositar(double quantidade) {
+	};
+
+	public double calculaRendimento() {
+		return 0.0;
+	};
+}

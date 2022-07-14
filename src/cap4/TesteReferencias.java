c09f05653e5aabaf2436adda1c744393654345ba
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-07-13 21:27:30 -0300

    5. Agora, crie duas referênciaspara a mesma conta e compare-ascomo ==.

--- src/cap4/TesteReferencias.java
+++ src/cap4/TesteReferencias.java
@@ -10,7 +10,13 @@ public class TesteReferencias {
 		c2.nomeTitular = "Danilo";
 		c2.saldo = 100.00;
 	
-		if (c1 == c2) {
+		Conta c3 = new Conta();
+		c3.nomeTitular = "Danilo";
+		c3.saldo = 100.00;
+		
+		Conta c4 = c3;
+		
+		if (c4 == c3) {
 			System.out.println("iguais");
 		} else {
 			System.out.println("diferentes");

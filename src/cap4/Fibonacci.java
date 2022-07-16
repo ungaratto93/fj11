5bf6d4778b6d2d6668d719afb5f9b6310064dcab
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-07-15 23:35:08 -0300

    3.	 Escreva	 o	 método	 recursivo	 novamente	 usando	 apenas	 uma	 linha.	 Para	 isso,	 pesquise	 sobre	 o operador	condicional	ternário	(ternary	operator

--- src/cap4/Fibonacci.java
+++ src/cap4/Fibonacci.java
@@ -1,15 +1,20 @@
 package cap4;
 
 public class Fibonacci {
-	int n1 = 1;
-	int n2 = -1;
+	private int n1 = 0;
+	private int n2 = 1;
 
+	private int sum1() {
+		n1 = n1 + n2;
+		return n1;
+	}
+	
+	private int sum2() {
+		n2 = n1 + n2;
+		return n2;
+	}
+	
 	public int calculaFibonnaci(int i) {
-		if (i % 2 == 0) {
-			n1 = n1 + n2;
-		} else {
-			n2 = n1 + n2;
-		}
-		return n1 + n2;
+		return (i % 2 == 0 )  ? sum1() : sum2();
 	}
 }

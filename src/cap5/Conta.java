ad29da8e6216dfc76c706ae55d1d9961955de46b
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-07-26 21:01:46 -0300

    2. Crie	apenas	os	getters	e	setters	necessários	na	sua	classe		Conta	.	Pense	 sempre	 se	é	 preciso	criar cada	um	deles.

--- src/cap5/Conta.java
+++ src/cap5/Conta.java
@@ -9,6 +9,38 @@ public class Conta {
 	private double saldo;
 	private Data dataAbertura = new Data();
 
+	public String getTitular() {
+		return this.titular;
+	}
+	
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+	
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public String getAgencia() {
+		return this.agencia;
+	}
+	
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+	
+	public String getDataAbertura() {
+		return this.dataAbertura.formatada();
+	}
+	
 	public void sacar(double quantidade) {
 		if (this.saldo > quantidade) {
 			this.saldo = this.saldo - quantidade;
---
+++ src/cap5/TestaConta.java
@@ -0,0 +1,10 @@
+package cap5;
+
+public class TestaConta {
+	public static void main(String[] args) {
+		
+		Conta c = new Conta();
+		
+		
+	}
+}
